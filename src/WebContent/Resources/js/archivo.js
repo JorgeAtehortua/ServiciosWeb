b2b88c6ddd1855b73a0aea78f546177d09b82a23
JorgeAtehortua <jorge.atehortua@gmail.com>
2015-07-29 03:51:09 -0500

    Proyecto con las Paginas HTML y JS comentadas

--- src/WebContent/Resources/js/archivo.js
+++ src/WebContent/Resources/js/archivo.js
@@ -1,40 +1,49 @@
-//Se crea el modulo y se intecta ngRoute que nos eprmite tener varias vistas
-//var appArchivos=angular.module('Archivo',[ 'ngRoute', 'ngCookies' ]);
+//Se crean las variables contenedoras de las URL relacionadas con los servicios web
 
 var URL_SERVICIO_OBTENER_ARCHIVOS = 'rest/Archivo/obtenerArchivos';
 var URL_SERVICIO_OBTENERPORCATEGORIA_ARCHIVOS = 'rest/Archivo/obtenerArchivoPorCategoria';
 var URL_SERVICIO_OBTENERPORCATEGORIAYTEMA_ARCHIVOS = 'rest/Archivo/obtenerArchivoPorCategoriaYTema';
-
 var URL_SERVICIO_OBTENERCATEGORIA_CATEGORIA = 'rest/Categoria/obtenerCategorias';
 var URL_SERVICIO_OBTENERTEMAPORCATEGORIA_TEMA = 'rest/Tema/obtenerTemasDeUnaCategoria';
 
-appUsuarios.controller('controllerObtenerArchivos', function($scope, Archivo,
+//Se crea controlador controllerObtenerArchivos el cual sera el encargado 
+//de procesar las diferentes tareas relacionadas con los archivos.
+
+app.controller('controllerObtenerArchivos', function($scope, Archivo,
 		$location) {
+	//funcion para consumir el servicio wed para obtenerArchivos.
 	$scope.obtenerArchivos = function() {
 		Archivo.obtenerArchivos().success(function(data) {
 			$scope.Archivos = data.archivoDto;
 		})
 	}
+	//funcion para consumir el servicio wed de obtenerArchivoPorCategoria
 	$scope.obtenerArchivoPorCategoria = function(idCategoria) {
 		Archivo.obtenerArchivoPorCategoria(idCategoria).success(function(data) {
 			$scope.categorias = data.archivoDto;
 		})
 	}
+	//funcion para consumir el servicio wed de obtenerCategorias
 	$scope.obtenerCategorias = function() {
 		Archivo.obtenerCategorias().success(function(data) {
 			$scope.categorias = data.categoriaDto;
 		})
 	}
+	//funcion para consumir el servicio wed de obtenerTemasPorCategoria
 	$scope.obtenerTemasPorCategoria = function(idCategoria) {
 		Archivo.obtenerTemasDeUnaCategoria(idCategoria).success(function(data) {
 			$scope.temas = data.temaDto;
 		})
 	}
+	//inicializacion de las funciones primarias.
 	$scope.obtenerArchivos();
 	$scope.obtenerCategorias();
 });
 
-appUsuarios.service('Archivo', function($http) {
+//Definicion de los diferentes servicios web para Archivo.
+
+app.service('Archivo', function($http) {
+	//Servicio web encargado de obtenerArchivos.
 	this.obtenerArchivos = function() {
 		return $http({
 			method : 'GET',
@@ -42,8 +51,7 @@ appUsuarios.service('Archivo', function($http) {
 
 		})
 	};
-
-	// Nombre del campo en el HTML idCategoria
+	//Servicio web encargado de obtenerArchivoPorCategoria.
 	this.obtenerArchivoPorCategoria = function(idCategoria) {
 		return $http({
 			method : 'GET',
@@ -54,7 +62,7 @@ appUsuarios.service('Archivo', function($http) {
 		})
 	};
 
-	// Nombre del campo en el HTML idCategoria, idTema
+	//Servicio web encargado de obtenerArchivoPorCategoriaYTema.
 	this.obtenerArchivoPorCategoriaYTema = function(idCategoria, idTema) {
 		return $http({
 			method : 'GET',
@@ -65,7 +73,7 @@ appUsuarios.service('Archivo', function($http) {
 			}
 		})
 	};
-
+	//Servicio web encargado de obtenerCategorias.
 	this.obtenerCategorias = function() {
 		return $http({
 			method : 'GET',
@@ -76,6 +84,7 @@ appUsuarios.service('Archivo', function($http) {
 
 		})
 	};
+	//Servicio web encargado de obtenerTemasDeUnaCategoria.
 	this.obtenerTemasDeUnaCategoria = function(idCategoria) {
 		return $http({
 			method : 'GET',
@@ -90,7 +99,8 @@ appUsuarios.service('Archivo', function($http) {
 
 });
 
-appUsuarios.config([ '$routeProvider', function($routeProvider) {
+//Configura las vistas del aplicativo
+app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/Archivos', {
 		templateUrl : 'archivos.html',
 		controller : 'controllerObtenerArchivos'
--- src/WebContent/Resources/js/usuario.js
+++ src/WebContent/Resources/js/usuario.js
@@ -1,63 +1,61 @@
 //Se crea el modulo y se le inyecta ngRoute que nos permite tener varias vistas
-var appUsuarios = angular.module('Usuario', [ 'ngRoute', 'ngCookies' ]);
+var app = angular.module('Usuario', [ 'ngRoute', 'ngCookies' ]);
 
+//creamos las variables en las cuales tendremos las URL relacionadas con los diferentes servicios.
 var URL_SERVICIO_LOGIN_USUARIO = 'rest/Usuario/Login';
 var URL_SERVICIO_GUARDAR_USUARIO = 'rest/Usuario/guardarUsuario';
 
-appUsuarios.controller('controllerLogin', function($scope, Usuario, $location) {
-	$scope.login = function() {
+//Definimos el controlador para login, el cual sera el encargado de manejar el proceso de validacion.
+app.controller('controllerLogin', function($scope, Usuario, $location){
+	$scope.login = function(){
 		// Estos son los nombres de los campos en el HTML
 		Usuario.validar($scope.idUsuario, $scope.contrasena).success(
-				function(data) {
-					if (!data.autenticado) {
-						$scope.idUsuario = '';
-						$scope.contrasena = '';
-						return;
-					} else
-					$location.path('/registroDeUsuarios');
-					
-				}
-				)
-	}
-})
-
-appUsuarios.controller('controllerPrincipal',function($scope, Usuario, $location) {
-	$scope.llamarLogin = function() {
-	
-		$location.path('/login');
-	}
-	
-			
-	
-	$scope.llamarArchivo = function() {
-
-		$location.path('/archivos')
-
+			function(data){
+				if (!data.autenticado) {
+					$scope.idUsuario = '';
+					$scope.contrasena = '';
+					return;
+				} else
+				$location.path('/registroDeUsuarios');
+			}
+		)
 	}
 });
-appUsuarios.controller('controllerRegistroUsuario', function($scope, Usuario, $location) {
-	$scope.guardarUsuario = function() {
-		
-		Usuario.guardarUsuario($scope.Usuario.idUsuario, $scope.Usuario.contrasena).success(
-				function(data) {
-					if (!data.guardado) {
-						$scope.Usuario.idUsuario = '';
-						$scope.Usuario.contrasena = '';
-						if (!data.datos) {
-							alert("Los campos son obligatorios");
-						}else alert("Ya existe un usuario con ese nombre");
-						
-						
-						return;
-					} else
-						alert("Usuario Registrado con exito!");
-					$location.path('/')
-				})
+
+//Definimos el controlador para la ventana principal.
+app.controller('controllerPrincipal',function($scope, Usuario, $location) {
+	$scope.llamarLogin = function() {
+		$location.path('/login');
 	}
-})
+	$scope.llamarArchivo = function() {
+		$location.path('/archivos');
+	}
+});
 
-appUsuarios.service('Usuario', function($http) {
+//Definimos el controlador para Registro de usuarios.
+app.controller('controllerRegistroUsuario', function($scope, Usuario, $location) {
+	$scope.guardarUsuario = function() {
+		Usuario.guardarUsuario($scope.Usuario.idUsuario, $scope.Usuario.contrasena).success(
+			function(data) {
+				if (!data.guardado) {
+					$scope.Usuario.idUsuario = '';
+					$scope.Usuario.contrasena = '';
+					if (!data.datos) {
+						alert("Los campos son obligatorios");
+					}else alert("Ya existe un usuario con ese nombre");
+					return;
+				} else
+					alert("Usuario Registrado con exito!");
+				$location.path('/');
+			})
+	}
+});
 
+//creacion del llamado al servicio Usuario, encargado de llamar los servicios de usuario.
+app.service('Usuario', function($http) {
+
+	//Servicio web para validar el login.
+	//Recibe el usuario y la contraseña ingresados.
 	this.validar = function(usuario, contrasena) {
 		return $http({
 			method : 'GET',
@@ -69,25 +67,23 @@ appUsuarios.service('Usuario', function($http) {
 		});
 	};
 
+	//Servicio web encargado de guardar un usuario nuevo.
+	//Recibe el usuario y la contraseña ingresados.
 	this.guardarUsuario = function(idUsuario, contrasena) {
-		
-		
 		return $http({
-			
 			method : 'POST',
 			url : URL_SERVICIO_GUARDAR_USUARIO,
 			params : {
 				idUsuario : idUsuario,
 				contrasena : contrasena
-				
-				
 			}
 		});
 	};
-
 });
+
+
 // Configura las vistas del aplicativo
-appUsuarios.config([ '$routeProvider', function($routeProvider) {
+app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/login', {
 		templateUrl : 'login.html',
 		controller : 'controllerLogin'
@@ -105,8 +101,7 @@ appUsuarios.config([ '$routeProvider', function($routeProvider) {
 		controller : 'controllerRegistroUsuario'
 	});
 	$routeProvider.otherwise({
-		templateUrl : 'registrarUsuario.html',
-		controller : 'controllerRegistroUsuario'
+		templateUrl : 'principal.html',
+		controller : 'controllerprincipal'
 	});
-
-} ]);
+}]);
